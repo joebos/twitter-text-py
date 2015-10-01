ccd94de049807a09d54da6231097ac33c541f22c
joebos <frank_lqx@yahoo.com>
2015-10-01 17:59:09 +0000

    updated http short url length to 23

--- twitter_text/validation.py
+++ twitter_text/validation.py
@@ -9,9 +9,9 @@ from twitter_text.regex import REGEXEN
 MAX_LENGTH = 140
 
 DEFAULT_TCO_URL_LENGTHS = {
-  'short_url_length': 22,
+  'short_url_length': 23,
   'short_url_length_https': 23,
-  'characters_reserved_per_media': 22,
+  'characters_reserved_per_media': 23,
 }
 
 class Validation(object):
